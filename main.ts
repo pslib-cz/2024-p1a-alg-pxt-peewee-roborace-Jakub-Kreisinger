d6b1c99336acc3c1b6aa69245cff3b5f678b415d
Jakub Kreisinger <jakub.kreisinger.024@pslib.cz>
2025-06-18 12:08:18 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -21,69 +21,71 @@ pins.setPull(IR.p, PinPullMode.PullNone);
 
 let L = 0;
 let P = 0;
+
+let dataL = 0;
+let dataC = 0;
+let dataP = 0;
+
 let intersection = false;
+let waitingForCommand = false;
 
-basic.forever(function () {
-    let dataL = pins.digitalReadPin(IR.l);
-    let dataC = pins.digitalReadPin(IR.c);
-    let dataP = pins.digitalReadPin(IR.p);
-
-    PCAmotor.MotorRun(PCAmotor.Motors.M1, L)
-    PCAmotor.MotorRun(PCAmotor.Motors.M4, P)
-
-    if (!intersection) {
-        if (dataC === 1 && dataL === 0 && dataP === 0) {
-            L = 200;
-            P = 200;
-        } else if (dataL === 1 && dataP === 0 && dataC === 0) {
-            L = 50;
-            P = 250;
-        } else if (dataL === 0 && dataP === 1 && dataC === 0) {
-            L = 250;
+radio.onReceivedString(function (received) {
+    if (waitingForCommand) {
+        if (received === "LEFT") {
+            // Odbočit doleva
+            L = 150;
             P = 50;
-        }
-    }
-
-    if (dataL === 1 && dataP === 1) {
-        radio.sendNumber(0);
-        L = 0;
-        P = 0;
-        intersection = true;
-    }
-    else if (dataC === 1 && dataP === 1) {
-        radio.sendNumber(0);
-        L = 0;
-        P = 0;
-        intersection = true;
-    }
-    else if (dataL === 1 && dataC === 1) {
-        radio.sendNumber(0);
-        L = 0;
-        P = 0;
-        intersection = true;
-    }
-    else if (dataL === 1 && dataP === 1 && dataC === 1) {
-        radio.sendNumber(0);
-        L = 0;
-        P = 0;
-        intersection = true;
-    }
-
-});
-
-radio.onReceivedNumber(function (receivedNumber: number) {
-    let serial = radio.receivedPacket(RadioPacketProperty.SerialNumber);
-    if (serial === 1569162800) {
-        if (receivedNumber === 1) {
+        } else if (received === "RIGHT") {
+            // Odbočit doprava
             L = 50;
-            P = 250;
-        } else if (receivedNumber === 2) {
-            L = 250;
-            P = 50;
-        } else if (receivedNumber === 3) {
-            L = 200;
-            P = 200;
+            P = 150;
+        } else if (received === "STRAIGHT") {
+            // Pokračovat rovně
+            L = 150;
+            P = -150;
         }
+        waitingForCommand = false;
         intersection = false;
     }
+});
+
+basic.forever(function () {
+    // Čtení senzorů
+    dataL = pins.digitalReadPin(IR.l);
+    dataC = pins.digitalReadPin(IR.c);
+    dataP = pins.digitalReadPin(IR.p);
+
+    if (!intersection && (dataL === 1 && dataC === 1 && dataP === 1)) {
+        // Detekce křižovatky
+        L = 0;
+        P = 0;
+        intersection = true;
+        waitingForCommand = true;
+        radio.sendString("WAITING");
+    }
+
+    // Řízení podle čáry (jen pokud nečeká na příkaz)
+    if (!waitingForCommand && !intersection) {
+        if (dataC === 0 && dataL === 1 && dataP === 1) {
+            // Jízda rovně
+            L = 150;
+            P = -150;
+        } else if (dataL === 0 && dataC === 1 && dataP === 1) {
+            // Mírně doprava
+            L = 50;
+            P = -150;
+        } else if (dataP === 0 && dataC === 1 && dataL === 1) {
+            // Mírně doleva
+            L = 150;
+            P = -50;
+        } else {
+            // Stop při ztrátě čáry
+            L = 0;
+            P = 0;
+        }
+    }
+
+    // Posílání příkazů motorům
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, L);
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, P);
 });
